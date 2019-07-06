a091317f1940112c4a0ccdcfcc3fda711d2c9632
Ann Kilzer <akilzer@gmail.com>
2019-07-06 20:44:01 +0900

    update tech page

--- src/views/Tech.vue
+++ src/views/Tech.vue
@@ -1,6 +1,86 @@
 <template>
   <single-card-page>
-    Coming soon!
+    <h1>Personal Projects</h1>
+    <p>
+      Check out my <a
+        target="_blank"
+        href="https://github.com/ann-kilzer"
+      >
+        GitHub page
+      </a>
+      to see my hobby projects. Most recently, I've been writing websites in Vue.js, and making whimsical
+      DApps in Solidity.
+    </p>
+
+    <h1>Press</h1>
+    <p>
+      <a
+        target="_blank"
+        href="https://journal.accj.or.jp/bias-buster/"
+      >
+        "Bias Buster." The Journal of the American Chamber of Commerce in Japan.
+        March 2019.
+      </a>
+    </p>
+    <h1>Publications &amp; Presentations</h1>
+    <p>
+      <ul>
+        <li>
+          "Canary in a coal mine: Building infrastructure resiliency with canary data reloads."
+          Velocity. San Jose, California. June 22, 2017.
+        </li>
+
+        <li>
+          Joseph A. Calandrino, Ann Kilzer, Arvind Narayanan, Edward W. Felten, Vitaly Shmatikov.
+          "You Might Also Like: Privacy Risks of Collaborative Filtering." S&amp;P. Oakland, CA:
+          IEEE, May 2011.
+          Press: The Economist Blog June 15, 2011.
+        </li>
+
+
+        <li>
+          Indrajit Roy, Srinath T.V. Setty, Ann Kilzer, Vitaly Shmatikov, Emmett Witchel."
+          Airavat: Security and Privacy for MapReduce." NSDI. San Jose, CA: USENIX, April 2010.
+        </li>
+
+        <li>
+          James Horey, Jean-Charles Tournier, Patrick Widener, Arthur B. Maccabe, Ann Kilzer.
+          "A Filesystem Interface for Sensor Networks." HotEmNets. Charlottesville, Virginia: ACM,
+          June 2008.
+        </li>
+      </ul>
+    </p>
+
+    <h1>Selected Work Experiences</h1>
+    <p>
+      <ul>
+        <li>Curvegrid: Lead Software Engineer (March 2018 - Present)</li>
+
+        <li>Indeed: Software Engineer (September 2014 to July 2017)</li>
+
+        <li>Boundary: Software Engineer (June 2013 to August 2014)</li>
+
+        <li>Telogis: Software Engineer (February 2012 to May 2013)</li>
+
+        <li>
+          The University of Texas at Austin: Research Assistant (August 2009 to August 2011)
+        </li>
+
+        <li>Google: Software Engineering Intern (May - August 2009)</li>
+
+        <li>Google: Platforms Software Engineering Intern (May - August 2008)</li>
+
+        <li>
+          Scalable Systems Lab at The University of New Mexico, Albuquerque, New Mexico:
+          Research Assistant (June - August 2007)
+        </li>
+
+        <li>
+          Department of Mathematics at California State University, San Bernardino: NSF Research
+          Experience for Undergraduates (July - August 2006)
+        </li>
+      </ul>
+    </p>
   </single-card-page>
 </template>
 
