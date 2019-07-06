ec9f8d2731af4c98786c7973f3103cea2e8dbb2c
Ann Kilzer <akilzer@gmail.com>
2019-07-06 14:41:14 +0900

    about me

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,28 @@
 <template>
-  <page-content>
-    <v-btn color="accent">
-      CLICK
-    </v-btn>
-  </page-content>
+  <single-card-page>
+    <p>
+      My name is Ann Kilzer. I'm a polymath, and my career is in software engineering.
+      I am the Lead Software Engineer at blockchain-as-a-service startup Curvegrid.
+      I received my Master's in Computer Science from the University of Texas at Austin,
+      where I spent a few years researching privacy. I grew up in Missoula, Montana, and
+      attended college at Gonzaga University, where I majored in computer science, math,
+      and art. I moved to Japan in early 2018 with no job, just an eager dream of living abroad.
+      In my spare time, I enjoy Japanese Calligraphy (Shodo), Nihonga painting, and
+      riding slow trains around Japan.
+    </p>
+    <p>
+      初めまして！杏マリーキルザーです。東京にすんでいます。日本画と書道を作る大好きです。
+      も電車大好きです🚂！よろしくお願いします。
+    </p>
+  </single-card-page>
 </template>
 
 <script>
-import PageContent from '../components/PageContent.vue';
+import SingleCardPage from '../components/SingleCardPage.vue';
 
 export default {
   components: {
-    PageContent,
+    SingleCardPage,
   },
 };
 </script>
