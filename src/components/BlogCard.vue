3f566d9594f2f2b9910a510d14ef8c1a9d19248a
Ann Kilzer <akilzer@gmail.com>
2019-08-19 00:09:04 +0900

    add blog card

---
+++ src/components/BlogCard.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="mb-4">
+    <v-card class="secondary lighten-2">
+      <v-container
+        pa-1
+      >
+        <v-card>
+          <v-container class="white">
+            <h1 class="secondary--text text--darken-2">
+              {{ title }}
+            </h1>
+            <v-divider class="mb-3 secondary lighten-3" />
+            <p>
+              <slot />
+            </p>
+          </v-container>
+        </v-card>
+      </v-container>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
